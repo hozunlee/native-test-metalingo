1f68461d2b579042e03f7c39816f47d27d459cc3
hozunlee <ho2yahh@gmail.com>
2022-12-24 14:34:14 +0900

    feat : home 목표 구현 및 컴포넌트 설계

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,97 +1,148 @@
-import { css } from '@emotion/native'
-import { useRef,useState } from 'react'
-import { Animated, Dimensions, Text, View } from 'react-native'
+import styled, { css } from '@emotion/native'
+import { useRef, useState } from 'react'
+import { Animated, Button, Dimensions, Text, View } from 'react-native'
 
 import { STATUS, SwipeablePanel } from '@app/components/SwipeablePanel'
+import WeeksPurpose from '@app/components/WeeksPurpose'
+import MyPurpose from '@app/components/MyPurpose'
 
 const HomeScreen: React.FC = () => {
 	const [panelStatus, setPanelStatus] = useState(2)
 	const panelHeight = Dimensions.get('window').height * 0.8
-    const minimizedHeight = Dimensions.get('window').height * 0.35
-    const opacityAnim = useRef(new Animated.Value(0.5)).current
+	const minimizedHeight = Dimensions.get('window').height * 0.35
+	const opacityAnim = useRef(new Animated.Value(0.5)).current
 
 	const [panelProps, setPanelProps] = useState({
-        fullWidth: true,
-        openLarge: true,        
-        canClose: false,
-        canScroll: false,
-        orientation: 'portrait',
-        showCloseButton: false,
-        largePanelHeight: panelHeight,
-        smallPanelHeight: minimizedHeight,
-        bigBackgroundOpacity: 0.0,
-        smallBackgroundOpacity: 0.0,
-        onChangeStatus: (status: STATUS) => changedStatus(status),
-        statusChangeDone: (status: STATUS) => statusChangeDone(status) 
-      });
+		fullWidth: true,
+		openLarge: true,
+		canClose: false,
+		canScroll: false,
+		orientation: 'portrait',
+		showCloseButton: false,
+		largePanelHeight: panelHeight,
+		smallPanelHeight: minimizedHeight,
+		bigBackgroundOpacity: 0.0,
+		smallBackgroundOpacity: 0.0,
+		onChangeStatus: (status: STATUS) => changedStatus(status),
+		statusChangeDone: (status: STATUS) => statusChangeDone(status),
+	})
 
-	  const [isPanelActive, setIsPanelActive] = useState(true);
+	const [isPanelActive, setIsPanelActive] = useState(true)
 
-	  const statusChangeDone = (status: STATUS) => {
-        setPanelStatus(status)
-      }
-	  const changedStatus = (status: STATUS) => {
-        if (status == 2) {
-            setPanelStatus(status)
-            Animated.timing(opacityAnim, {
-                toValue: 0.6,
-                duration: 700,
-                useNativeDriver: true
-            }).start()
-        } else {
-            Animated.timing(opacityAnim, {
-                toValue: 0.0,
-                duration: 700,
-                useNativeDriver: true
-            }).start()
-        }
-    } 
-
+	const statusChangeDone = (status: STATUS) => {
+		setPanelStatus(status)
+	}
+	const changedStatus = (status: STATUS) => {
+		if (status == 2) {
+			setPanelStatus(status)
+			Animated.timing(opacityAnim, {
+				toValue: 0.6,
+				duration: 700,
+				useNativeDriver: true,
+			}).start()
+		} else {
+			Animated.timing(opacityAnim, {
+				toValue: 0.0,
+				duration: 700,
+				useNativeDriver: true,
+			}).start()
+		}
+	}
 
 	// static data to build front-end UI
 	const userData = {
 		targetMinutes: 20,
 		targetDaysPerWeek: 6,
-		targetDaysPerMonth: 20
+		targetDaysPerMonth: 20,
 	}
 	const data = {
 		todayMinutes: 6,
 		currentDays: 5,
 	}
 
+	const todayPurpose = userData.targetMinutes - data.todayMinutes
 
 	return (
-		
 		<View
 			style={css`
 				flex: 1;
-				background: #000; 
+				background: #000;
 			`}
 		>
 			<SwipeablePanel {...panelProps} isActive={isPanelActive}>
-                    <View style={[css`
-                        padding: 24px;
-                        padding-bottom: 50px;
-                        padding-top: 30px;
-                        flex-direction: column;
-                    `, { height: panelHeight  }]}>
-                        <Text style={css`font-size: 18px; text-align: center`}>이번 주에 4일 연속으로 접속하셨어요.</Text>
-                        <View style={[css`
-                            flex: 0.3;
-                            flex-direction: row;
-                            justify-content: flex-end;
-                            // background-color: red;
-                        `]}>
-                            {/* <MBText>{JSON.stringify(greetings.data)}</MBText> */}
-                            {/* Enter statistics */}
-				        </View>
-                    </View>
-                </SwipeablePanel> 
-			
-			
+				<View
+					style={[
+						css`
+							padding: 24px;
+							padding-bottom: 50px;
+							padding-top: 30px;
+							flex-direction: column;
+						`,
+						{ height: panelHeight },
+					]}
+				>
+					<Text
+						style={css`
+							font-size: 18px;
+							text-align: center;
+						`}
+					>
+						이번 주에 4일 연속으로 접속하셨어요.
+					</Text>
+					<Text
+						style={css`
+							font-size: 18px;
+							text-align: center;
+							margin-bottom: 50px;
+						`}
+					>
+						{`${todayPurpose}분 더 대화하시면 오늘의 목표가 달성돼요!`}
+					</Text>
+					<View
+						style={[
+							css`
+								flex: 0.3;
+								/* flex-direction: row; */
+								/* justify-content: flex-end; */
+								// background-color: red;
+							`,
+						]}
+					>
+						{/* <MBText>{JSON.stringify(greetings.data)}</MBText> */}
+						{/* Enter statistics */}
+						<WeeksPurpose />
+					</View>
+					<MyPurposeContainer>
+						<MyPurpose
+							title="TODAY"
+							doing={data.todayMinutes}
+							purpose={userData.targetMinutes}
+						/>
+						<MyPurpose
+							title="WEEK"
+							doing={data.currentDays}
+							purpose={userData.targetDaysPerWeek}
+						/>
+						<MyPurpose
+							title="MONTH"
+							doing={data.currentDays}
+							purpose={userData.targetDaysPerMonth}
+						/>
+					</MyPurposeContainer>
+					<Button title="시작하기" />
+				</View>
+			</SwipeablePanel>
 		</View>
-
 	)
 }
 
 export default HomeScreen
+
+const MyPurposeContainer = styled.View`
+	/* flex: 1; */
+	padding-top: 30px;
+	flex-direction: row;
+	/* border: 1px solid red; */
+	justify-content: space-around;
+	/* align-items: center; */
+`
